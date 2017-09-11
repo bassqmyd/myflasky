4eb8cf950ccf11cf9bb46dba8094dbc1073b15fb
chen_meng_dan <945581235@qq.com>
2017-09-11 20:19:26 +0800

    在视图函数中处理表单，获取POST请求中提交的表单数据

--- app1/hello.py
+++ app1/hello.py
@@ -3,16 +3,31 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
+@app.route('/', methods=['get', 'post'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name,
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
