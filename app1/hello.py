606460cd4fb71a4ec4575089d4e4632839e1bc15
chen_meng_dan <945581235@qq.com>
2017-09-13 21:01:51 +0800

    异步发送电子邮件，避免处理请求过程中不必要的延迟， 把发送电子邮件的函数移到后台线程中。

--- app1/hello.py
+++ app1/hello.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
 import os
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -44,14 +45,20 @@ class NameForm(FlaskForm):
     submit = SubmitField('submit')
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'],
                   recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    with app.app_context():
-        mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 @app.route('/', methods=['get', 'post'])
